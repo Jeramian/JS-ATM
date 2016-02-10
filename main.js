b646e010244b563d4027482949d7838d6efffae4
Jeremy Eramian <jeremyleramian@outlook.com>
2016-02-10 14:35:11 -0800

    going places

--- main.js
+++ main.js
@@ -1,11 +1,42 @@
+var balance;
+
+var info = document.createElement('p');
+var balanceText = document.createTextNode('Your current balance is:', balance);
+info.appendChild(balanceText);
+
+function redirect()
+{
+	var loggedUrl = "http://jeramian.github.io/JS-ATM/logged.html";
+	document.location.href = loggedUrl;
+}
+function errorAlert()
+{
+	alert("Something went wrong! Please ask for assistance");
+}
+function success()
+{
+	alert("Hey! Im working");
+}
+function logoutRedirect()
+{
+	var logoutUrl = "index.html";
+	document.location.href = logoutUrl;
+}
+function balance()
+{
+
+}
+function withdrawl()
+{
+
+}
+function deposit()
+{
+
+}
+
 function ATM()
 {
-	function redirect()
-	{
-		var loggedUrl = "logged.html";
-		window.location(url);
-	}
-	
 	event.preventDefault();
 	var accounts = {cardNumber:"123456", pin:"123", balance:"$12.00"};
 
@@ -18,7 +49,33 @@ function ATM()
 		}
 	else
 		{
-			alert("Something went wrong! Please ask for assistance");
+			errorAlert();
 		}
+}
 
+function optionSelect()
+{
+	event.preventDefault();
+	var menu =  document.getElementById('optSel').value;
+
+	if(menu == 1)
+	{
+		success();
+	}
+	else if(menu == 2)
+	{
+		success();
+	}
+	else if(menu == 3)
+	{
+		success();
+	}
+	else if(menu == 4)
+	{
+		success();
+	}
+	else
+	{
+		errorAlert();
+	}
 }
